d325ed8f4f614e8b74e79576c07a37c052b83eb4
EvelynDCG <78814811+EvelynDCG@users.noreply.github.com>
2022-05-16 19:13:57 -0700

    16-05-22-Se creo el form para agregar elementos a menu

---
+++ POS/agregarMenuForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace POS
+{
+    partial class agregarMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nombreLabel = new System.Windows.Forms.Label();
+            this.seccionLabel = new System.Windows.Forms.Label();
+            this.precioLabel = new System.Windows.Forms.Label();
+            this.descripcionLabel = new System.Windows.Forms.Label();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.precioTextBox = new System.Windows.Forms.TextBox();
+            this.seccionComboBox = new System.Windows.Forms.ComboBox();
+            this.descripcionRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.encabezadoLabel = new System.Windows.Forms.Label();
+            this.encabezadoPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.encabezadoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nombreLabel
+            // 
+            this.nombreLabel.AutoSize = true;
+            this.nombreLabel.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombreLabel.Location = new System.Drawing.Point(53, 183);
+            this.nombreLabel.Name = "nombreLabel";
+            this.nombreLabel.Size = new System.Drawing.Size(77, 19);
+            this.nombreLabel.TabIndex = 0;
+            this.nombreLabel.Text = "NOMBRE:";
+            // 
+            // seccionLabel
+            // 
+            this.seccionLabel.AutoSize = true;
+            this.seccionLabel.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seccionLabel.Location = new System.Drawing.Point(53, 234);
+            this.seccionLabel.Name = "seccionLabel";
+            this.seccionLabel.Size = new System.Drawing.Size(77, 19);
+            this.seccionLabel.TabIndex = 1;
+            this.seccionLabel.Text = "SECCIÓN:";
+            // 
+            // precioLabel
+            // 
+            this.precioLabel.AutoSize = true;
+            this.precioLabel.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.precioLabel.Location = new System.Drawing.Point(297, 234);
+            this.precioLabel.Name = "precioLabel";
+            this.precioLabel.Size = new System.Drawing.Size(65, 19);
+            this.precioLabel.TabIndex = 2;
+            this.precioLabel.Text = "PRECIO:";
+            // 
+            // descripcionLabel
+            // 
+            this.descripcionLabel.AutoSize = true;
+            this.descripcionLabel.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descripcionLabel.Location = new System.Drawing.Point(53, 284);
+            this.descripcionLabel.Name = "descripcionLabel";
+            this.descripcionLabel.Size = new System.Drawing.Size(111, 19);
+            this.descripcionLabel.TabIndex = 3;
+            this.descripcionLabel.Text = "DESCRIPCIÓN:";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombreTextBox.Location = new System.Drawing.Point(145, 180);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(323, 29);
+            this.nombreTextBox.TabIndex = 4;
+            // 
+            // precioTextBox
+            // 
+            this.precioTextBox.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.precioTextBox.Location = new System.Drawing.Point(368, 226);
+            this.precioTextBox.Name = "precioTextBox";
+            this.precioTextBox.Size = new System.Drawing.Size(100, 29);
+            this.precioTextBox.TabIndex = 5;
+            // 
+            // seccionComboBox
+            // 
+            this.seccionComboBox.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seccionComboBox.FormattingEnabled = true;
+            this.seccionComboBox.Items.AddRange(new object[] {
+            "PLATILLO",
+            "BEBIDA",
+            "POSTRE"});
+            this.seccionComboBox.Location = new System.Drawing.Point(136, 226);
+            this.seccionComboBox.Name = "seccionComboBox";
+            this.seccionComboBox.Size = new System.Drawing.Size(121, 27);
+            this.seccionComboBox.TabIndex = 6;
+            // 
+            // descripcionRichTextBox
+            // 
+            this.descripcionRichTextBox.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descripcionRichTextBox.Location = new System.Drawing.Point(57, 306);
+            this.descripcionRichTextBox.Name = "descripcionRichTextBox";
+            this.descripcionRichTextBox.Size = new System.Drawing.Size(412, 171);
+            this.descripcionRichTextBox.TabIndex = 7;
+            this.descripcionRichTextBox.Text = "";
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Location = new System.Drawing.Point(23, 23);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.logoPictureBox.TabIndex = 8;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // encabezadoLabel
+            // 
+            this.encabezadoLabel.AutoSize = true;
+            this.encabezadoLabel.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encabezadoLabel.ForeColor = System.Drawing.Color.White;
+            this.encabezadoLabel.Location = new System.Drawing.Point(201, 7);
+            this.encabezadoLabel.Name = "encabezadoLabel";
+            this.encabezadoLabel.Size = new System.Drawing.Size(201, 25);
+            this.encabezadoLabel.TabIndex = 9;
+            this.encabezadoLabel.Text = "AGREGAR A MENÚ";
+            // 
+            // encabezadoPanel
+            // 
+            this.encabezadoPanel.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.encabezadoPanel.Controls.Add(this.encabezadoLabel);
+            this.encabezadoPanel.Location = new System.Drawing.Point(1, 99);
+            this.encabezadoPanel.Name = "encabezadoPanel";
+            this.encabezadoPanel.Size = new System.Drawing.Size(581, 38);
+            this.encabezadoPanel.TabIndex = 10;
+            // 
+            // agregarMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 497);
+            this.Controls.Add(this.encabezadoPanel);
+            this.Controls.Add(this.logoPictureBox);
+            this.Controls.Add(this.descripcionRichTextBox);
+            this.Controls.Add(this.seccionComboBox);
+            this.Controls.Add(this.precioTextBox);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(this.descripcionLabel);
+            this.Controls.Add(this.precioLabel);
+            this.Controls.Add(this.seccionLabel);
+            this.Controls.Add(this.nombreLabel);
+            this.Name = "agregarMenuForm";
+            this.Text = "agregarMenuForm";
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.encabezadoPanel.ResumeLayout(false);
+            this.encabezadoPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nombreLabel;
+        private System.Windows.Forms.Label seccionLabel;
+        private System.Windows.Forms.Label precioLabel;
+        private System.Windows.Forms.Label descripcionLabel;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.TextBox precioTextBox;
+        private System.Windows.Forms.ComboBox seccionComboBox;
+        private System.Windows.Forms.RichTextBox descripcionRichTextBox;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Label encabezadoLabel;
+        private System.Windows.Forms.Panel encabezadoPanel;
+    }
+}
--- POS/altaMenuForm.Designer.cs
+++ POS/altaMenuForm.Designer.cs
@@ -36,18 +36,21 @@ namespace POS
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(165, 47);
+            this.label1.Location = new System.Drawing.Point(124, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(439, 52);
+            this.label1.Size = new System.Drawing.Size(347, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "AGREGAR A MENU";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // altaMenuForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "altaMenuForm";
             this.Text = "altaMenuForm";
             this.ResumeLayout(false);
