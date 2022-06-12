014314c1ee243a7722abe1f367cf6b0690399b29
Gabriel Gallegos <gabriel.gallegos@uabc.edu.mx>
2022-06-11 20:57:58 -0700

    Gabriel_11_06_2022_2
    Volví a colocar el código para dar de alta en los nuevos botones redondeados y agregué código para la consulta de elementos. Está comentado en los archivos BLConsultaElemento y consultaMenuForm.cs

--- POS/BLConsultaElemento.cs
+++ POS/BLConsultaElemento.cs
@@ -8,6 +8,28 @@ namespace POS
 {
     public class BLConsultaElemento
     {
+        //public static List<dataLayer.elemento> elementosGet()
+        //{
+        //    try
+        //    {
+        //        using (dataLayer.POS_BDEntities db = new dataLayer.POS_BDEntities())
+        //        {
+        //            var query = (from a in db.elemento
+        //                         select new dataLayer.elemento
+        //                         {
+        //                             nombre_elemento = a.nombre_elemento,
+        //                             precio = a.precio,
+        //                             seccion = a.seccion
 
+        //                         }).ToList();
+        //            return query;
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+        //}
     }
 }
--- POS/agregarMenuForm.cs
+++ POS/agregarMenuForm.cs
@@ -22,35 +22,7 @@ namespace POS
 
         private void agregarButton_Click(object sender, EventArgs e)
         {
-            if (nombreTextBox.Text.Equals(""))
-            {
-                MessageBox.Show("¡No se ha ingresado el nombre del elemento!", "Dato requerido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else {
-                if (seccionComboBox.Text.Equals(""))
-                {
-                    MessageBox.Show("¡No se ha ingresado la sección del elemento!", "Dato requerido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-                else {
-                    if (precioTextBox.Text.Equals(""))
-                    {
-                        MessageBox.Show("¡No se ha ingresado el precio del elemento!", "Dato requerido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
-                    else {
-                        double precio;
-                        precio = Convert.ToDouble(precioTextBox.Text);
-                        try
-                        {
-                            BLAgregarElemento.agregarElemento(nombreTextBox.Text, seccionComboBox.Text, precio, descripcionRichTextBox.Text);
-                            MessageBox.Show("¡Se ha dado de alta con exito!", "Alta de elemento", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            this.Close();
-                        }
-                        catch(Exception ex) {
-                            MessageBox.Show("¡Ha ocurrido un error al dar de alta el elemento!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        } 
-                    }
-                }
-            }
+            
         }
 
         private void agregarMenuForm_Load(object sender, EventArgs e)
@@ -84,6 +56,43 @@ namespace POS
         private void cancelarButton_Click_1(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void agregarButton_Click_1(object sender, EventArgs e)
+        {
+            if (nombreTextBox.Text.Equals(""))
+            {
+                MessageBox.Show("¡No se ha ingresado el nombre del elemento!", "Dato requerido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (seccionComboBox.Text.Equals(""))
+                {
+                    MessageBox.Show("¡No se ha ingresado la sección del elemento!", "Dato requerido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    if (precioTextBox.Text.Equals(""))
+                    {
+                        MessageBox.Show("¡No se ha ingresado el precio del elemento!", "Dato requerido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        double precio;
+                        precio = Convert.ToDouble(precioTextBox.Text);
+                        try
+                        {
+                            BLAgregarElemento.agregarElemento(nombreTextBox.Text, seccionComboBox.Text, precio, descripcionRichTextBox.Text);
+                            MessageBox.Show("¡Se ha dado de alta con exito!", "Alta de elemento", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("¡Ha ocurrido un error al dar de alta el elemento!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
         }
     }
 }
