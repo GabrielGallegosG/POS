fbd357707af5f8e1c0a41e444381e235227d9154
Gabriel Gallegos <gabriel.gallegos@uabc.edu.mx>
2022-06-13 22:00:10 -0700

    Gabriel_13_06_2022_5
    Comentados los errores que daban en el form de consultaOrdenesForm

--- POS/consultaOrdenesForm.cs
+++ POS/consultaOrdenesForm.cs
@@ -12,29 +12,30 @@ namespace POS
 {
     public partial class consultaOrdenesForm : Form
     {
-        public consultaOrdenesForm()
-        {
-            InitializeComponent();
-            PLConsultaOrdenes.posicionEncabezado(inicioBbutton, menuButton, ordenesButton, ventasButton, usuariosButton);
-            PLConsultaOrdenes.posisiconConsultaOrdenes(logoPictureBox,encabezadoLabel,nuevaOrdenLabel,agregarOrdenButton);
-        }
+        //    public consultaOrdenesForm()
+        //    {
+        //        InitializeComponent();
+        //        PLConsultaOrdenes.posicionEncabezado(inicioBbutton, menuButton, ordenesButton, ventasButton, usuariosButton);
+        //        PLConsultaOrdenes.posisiconConsultaOrdenes(logoPictureBox,encabezadoLabel,nuevaOrdenLabel,agregarOrdenButton);
+        //    }
 
-        private void ordenesButton_Click(object sender, EventArgs e)
-        {
-            consultaOrdenesForm frm = new consultaOrdenesForm();
-            frm.Show();
-        }
+        //    private void ordenesButton_Click(object sender, EventArgs e)
+        //    {
+        //        consultaOrdenesForm frm = new consultaOrdenesForm();
+        //        frm.Show();
+        //    }
 
-        private void menuButton_Click(object sender, EventArgs e)
-        {
-            consultaMenuForm frm = new consultaMenuForm();
-            frm.Show();
-        }
+        //    private void menuButton_Click(object sender, EventArgs e)
+        //    {
+        //        consultaMenuForm frm = new consultaMenuForm();
+        //        frm.Show();
+        //    }
 
-        private void agregarOrdenButton_Click(object sender, EventArgs e)
-        {
-            agregarOrdenForm frm = new agregarOrdenForm();
-            frm.Show();
-        }
+        //    private void agregarOrdenButton_Click(object sender, EventArgs e)
+        //    {
+        //        agregarOrdenForm frm = new agregarOrdenForm();
+        //        frm.Show();
+        //    }
+        //}
     }
 }
