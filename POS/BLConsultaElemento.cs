cc9433f24aa5af3d08e46153f7b79554b0a998a7
ClaudiaArias01 <arias.claudia8@uabc.edu.mx>
2022-06-13 21:45:55 -0700

    cambios en consulta y en recursos

--- POS/BLConsultaElemento.cs
+++ POS/BLConsultaElemento.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,28 +10,60 @@ namespace POS
 {
     public class BLConsultaElemento
     {
-        //public static List<dataLayer.elemento> elementosGet()
-        //{
-        //    try
-        //    {
-        //        using (dataLayer.POS_BDEntities db = new dataLayer.POS_BDEntities())
-        //        {
-        //            var query = (from a in db.elemento
-        //                         select new dataLayer.elemento
-        //                         {
-        //                             nombre_elemento = a.nombre_elemento,
-        //                             precio = a.precio,
-        //                             seccion = a.seccion
+        public static DataTable PlatillosDT()
+        {
+            try { 
+                SqlConnection conexion = new SqlConnection("server=desktop-arias-r\\mssqlserver01 ; database=POS_BD ; integrated security = SSPI");
+                conexion.Open();
 
-        //                         }).ToList();
-        //            return query;
-        //        }
+                string consulPla = "select nombre_elemento,precio from elemento where seccion = '1'";
+                SqlDataAdapter adaptPla = new SqlDataAdapter(consulPla, conexion);
+                DataTable dtPlatillo = new DataTable();
+                adaptPla.Fill(dtPlatillo);
+                return dtPlatillo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
 
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
+        }
+
+        public static DataTable BebidasDT()
+        {
+            try { 
+                SqlConnection conexion = new SqlConnection("server=desktop-arias-r\\mssqlserver01 ; database=POS_BD ; integrated security = SSPI");
+                conexion.Open();
+
+                string consulBe = "select nombre_elemento,precio from elemento where seccion = '2'";
+                SqlDataAdapter adaptBe = new SqlDataAdapter(consulBe, conexion);
+                DataTable dtBebida = new DataTable();
+                adaptBe.Fill(dtBebida);
+                return dtBebida;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static DataTable PostresDT()
+        {
+            try { 
+                SqlConnection conexion = new SqlConnection("server=desktop-arias-r\\mssqlserver01 ; database=POS_BD ; integrated security = SSPI");
+                conexion.Open();
+
+                string consulPos = "select nombre_elemento,precio from elemento where seccion = '3'";
+                SqlDataAdapter adaptPos = new SqlDataAdapter(consulPos, conexion);
+                DataTable dtPostre = new DataTable();
+                adaptPos.Fill(dtPostre);
+
+                return dtPostre;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
-}
+} 
